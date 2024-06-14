2b96867b1557b76380c18ace43fad110145302e4
Vivian <vivian.culn@windriver.com>
2024-06-14 06:15:31 -0300

    clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import { useEffect } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import AppAppBar from './components/AppAppBar';
 import Hero from './components/Hero';
 import getLPTheme from './styles/getLPTheme';
-import web3 from './web3/web3';
 
 export default function LandingPage() {
   const LPtheme = createTheme(getLPTheme('dark'));
 
-  const showAccount= async () => {
-    const account = await web3.eth.getAccounts();
-    console.log(account);
-  }
-
-  useEffect(() => {
-    showAccount()
-  },[])
-
   return (
     <ThemeProvider theme={LPtheme}>
       <CssBaseline />
-      {/* <AppAppBar /> */}
       <Hero />
     </ThemeProvider>
   );
